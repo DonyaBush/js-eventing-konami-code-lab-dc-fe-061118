510ef8f5b52a842ff3e651e779264e7c98093534
Ki Ki  Bush <donyabush@gmail.com>
2018-07-31 01:01:01 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -14,7 +14,7 @@ let index = 0;
 // This is the function that would be invoked by the event listener.
 function onKeyDownHandler(e) {
   const key = parseInt(e.detail || e.which);
- 
+ console.log(key)
   if (key === alphabet[index]) {
     index++;
  
